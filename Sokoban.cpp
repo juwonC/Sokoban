00bff9240b77dfe25201457c005fb7466f1e4e30
JuwonCha <juwon1512@gmail.com>
2022-03-24 14:59:11 +0900

    초기 커밋.

---
+++ Sokoban.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+
+const char stageData[] = 
+"\
+########\n\
+# .. p #\n\
+# oo   #\n\
+#      #\n\
+########";
+
+const int stageWidth{ 8 };
+const int stageHeight{ 5 };
+
+int main()
+{
+    while (true)
+    {
+        그리기();
+        if (게임종료())
+            break;
+
+        입력처리;
+        업데이트();
+    }
+}
+
+초기화()
+{
+    while (배열전체)
+    {
+
+    }
+}
+
+그리기()
+{
+
+}
+
+게임종료()
+{
+
+}
+
+업데이트()
+{
+
+}
