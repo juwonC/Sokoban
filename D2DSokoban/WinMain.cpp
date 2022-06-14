d3d0d87ad69895db280bda739e7922300e10f777
JuwonCha <juwon1512@gmail.com>
2022-06-14 23:29:53 +0900

    Feat Direct2D

--- D2DSokoban/WinMain.cpp
+++ D2DSokoban/WinMain.cpp
@@ -1,8 +1,14 @@
 #include <windows.h>
+#include <d2d1.h>
+
+#pragma comment (lib, "d2d1.lib")
 
 const wchar_t gClassName[] = L"MyWindow";
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+ID2D1Factory* gpD2DFactory{};
+ID2D1HwndRenderTarget* gpRenderTarget{};
 
 int WINAPI WinMain(
 	_In_ HINSTANCE hInstance,
@@ -10,6 +16,13 @@ int WINAPI WinMain(
 	_In_ LPSTR lpCmdLine,
 	_In_ int nShowCmd )
 {
+	HRESULT hr = D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, &gpD2DFactory);
+	if (FAILED(hr))
+	{
+		MessageBox(NULL, L"Failed to create D2D Factory", L"Error", MB_ICONEXCLAMATION | MB_OK);
+		return 0;
+	}
+
 	HWND hwnd;
 	WNDCLASSEX wc;
 
@@ -51,6 +64,21 @@ int WINAPI WinMain(
 		return 0;
 	}
 
+	GetClientRect(hwnd, &rct);
+	hr = gpD2DFactory->CreateHwndRenderTarget(
+		D2D1::RenderTargetProperties(),
+		D2D1::HwndRenderTargetProperties(
+			hwnd,
+			D2D1::SizeU(rct.right - rct.left, rct.bottom - rct.top)
+		),
+		&gpRenderTarget
+	);
+	if (FAILED(hr))
+	{
+		MessageBox(NULL, L"Failed to create D2D RenderTarget", L"Error", MB_ICONEXCLAMATION | MB_OK);
+		return 0;
+	}
+
 	ShowWindow(hwnd, nShowCmd);
 	UpdateWindow(hwnd);
 
@@ -59,6 +87,17 @@ int WINAPI WinMain(
 	{
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
+	}
+
+	if (gpRenderTarget != nullptr)
+	{
+		gpRenderTarget->Release();
+		gpRenderTarget = nullptr;
+	}
+	if (gpD2DFactory != nullptr)
+	{
+		gpD2DFactory->Release();
+		gpD2DFactory = nullptr;
 	}
 
 	return static_cast<int>(msg.wParam);
@@ -70,6 +109,10 @@ void OnPaint(HWND hwnd)
 	PAINTSTRUCT ps;
 
 	hdc = BeginPaint(hwnd, &ps);
+
+	gpRenderTarget->BeginDraw();
+	gpRenderTarget->Clear(D2D1::ColorF(0.0f, 0.2f, 0.4f, 1.0f));
+	gpRenderTarget->EndDraw();
 
 	EndPaint(hwnd, &ps);
 }
